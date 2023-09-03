d161daa980f37cf3e780195797d8a38d7dd3df00
chistyakova <chistyakova@umarsh.ru>
2023-09-03 18:41:20 +0300

    Реализация задания 12.3.3

---
+++ src/ChatServer.java
@@ -0,0 +1,35 @@
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+
+public class ChatServer {
+    ArrayList<Client> clients = new ArrayList<>();
+    ServerSocket serverSocket;
+    ChatServer() throws IOException {
+        serverSocket = new ServerSocket(1234);
+    }
+    void sendAll(String s){
+        for (Client cline : clients){
+            cline.receive(s);
+        }
+    }
+    public void run() throws IOException {
+        while(true) {
+            System.out.println("Waiting...");
+            // ждем клиента из сети
+            try {
+                Socket socket = serverSocket.accept();
+                System.out.println("Client connected!");
+                // создаем клиента на своей стороне
+                clients.add(new Client(socket, this));
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+    public static void main(String[] args) throws IOException {
+        new ChatServer().run();
+    }
+}
